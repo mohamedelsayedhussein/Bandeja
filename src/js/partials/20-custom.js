0bc13fdf0da96b5b2b290b6b32ed6d804789eba9
mohamed elsayed <mohamedelsayedhussein0@gmail.com>
2019-12-07 13:48:35 +0200

    fff

--- src/js/partials/20-custom.js
+++ src/js/partials/20-custom.js
@@ -1,60 +1,97 @@
 
-var $slider = $('.slider');
-var $slickTrack = $slider.find($('.slick-track'));
-var $slickCurrent = $slider.find($('.slick-current'));
-var $slickList = $slider.find($('.slick-list'));
+var $imageSlider = $('.image-slider');
+var $textSlider = $('.text-slider');
 var slideDuration = 900;
-// var tlSlider = new TimelineMax()
 
 //RESET ANIMATIONS
 // On init slide change
-$slider.on('init', function(slick){
-  TweenMax.to($('.slick-track'), 0.9, { marginLeft: 0});
-  TweenMax.to($('.slick-active'), 0.9, { x: 0,zIndex: 2});
+$imageSlider.on('init', function(slick){
+  TweenMax.to($('.image-slider .slick-track'), 0.9, { marginLeft: 0});
+  TweenMax.to($('.image-slider .slick-active'), 0.9, { x: 0,zIndex: 2});
 });
 // On before slide change
-$slider.on('beforeChange', function(event, slick, currentSlide, nextSlide){
-  TweenMax.to($('.slick-track'), 0.9, {marginLeft: 0});
-  TweenMax.to($('.slick-active'), 0.9, {x: 0});
-  $('.slick-list').addClass('do-transition')
+$imageSlider.on('beforeChange', function(event, slick, currentSlide, nextSlide){
+  TweenMax.to($('.image-slider .slick-track'), 0.9, {marginLeft: 0});
+  TweenMax.to($('.image-slider .slick-active'), 0.9, {x: 0});
+  $('.image-slider .slick-list').addClass('do-transition')
 });
 
 // On after slide change
-$slider.on('afterChange', function(event, slick, currentSlide){
-  TweenMax.to($('.slick-track'), 0.9, {marginLeft: 0});
-  $('.slick-slide').css('z-index','1');
-  TweenMax.to($('.slick-active'), 0.9, {x: 0,zIndex: 2});
-  $('.slick-list').removeClass('do-transition')
+$imageSlider.on('afterChange', function(event, slick, currentSlide){
+  TweenMax.to($('.image-slider .slick-track'), 0.9, {marginLeft: 0});
+  $('.image-slider .slick-slide').css('z-index','1');
+  TweenMax.to($('.image-slider .slick-active'), 0.9, {x: 0,zIndex: 2});
+  $('.image-slider .slick-list').removeClass('do-transition')
 });
 
 //SLICK INIT
-$slider.slick({
+$imageSlider.slick({
+  autoplay: true,
+  slidesToScroll: 1,
+  slidesToShow: 1,
   speed: slideDuration,
   dots: true,
   waitForAnimate: true,
+  pauseOnHover: false,
   useTransform: true,
+  asNavFor: $textSlider,
   cssEase: 'cubic-bezier(0.84, 0, 0.08, 0.99)'
+})
+
+$textSlider.slick({
+  autoplay: true,
+  slidesToScroll: 1,
+  slidesToShow: 1,
+  speed: slideDuration,
+  pauseOnHover: false,
+  asNavFor: $imageSlider,
+  arrows: false,
+  dots: false,
+  cssEase: 'cubic-bezier(0.84, 0, 0.08, 0.99)',
 })
 
 //PREV
 $('.slick-prev').on('mouseenter', function(){
-  TweenMax.to($('.slick-track'), 0.6, {marginLeft: "180px",ease: Quad.easeOut});
-  TweenMax.to($('.slick-current'), 0.6, {x: -100,ease: Quad.easeOut});
+  TweenMax.to($('.image-slider .slick-track'), 1.5, {marginLeft: "180px",ease: Elastic.easeOut});
+  TweenMax.to($('.image-slider .slick-current'), 1.5, {x: -100,ease: Elastic.easeOut});
 });
 
 $('.slick-prev').on('mouseleave', function(){
-  TweenMax.to($('.slick-track'), 0.4, {marginLeft: 0,ease: Sine.easeInOut});
-  TweenMax.to($('.slick-current'), 0.4, {x: 0,ease: Sine.easeInOut});
+  TweenMax.to($('.image-slider .slick-track'), 0.4, {marginLeft: 0,ease: Sine.easeInOut});
+  TweenMax.to($('.image-slider .slick-current'), 0.4, {x: 0,ease: Sine.easeInOut});
 });
 
 //NEXT
 $('.slick-next').on('mouseenter', function(){  
-  TweenMax.to($('.slick-track'), 0.6, {marginLeft: "-180px",ease: Quad.easeOut});
-  TweenMax.to($('.slick-current'), 0.6, {x: 100,ease: Quad.easeOut});
+  TweenMax.to($('.image-slider .slick-track'), 1.5, {marginLeft: "-180px",ease: Elastic.easeOut});
+  TweenMax.to($('.image-slider .slick-current'), 1.5, {x: 100,ease: Elastic.easeOut});
 });
 
 $('.slick-next').on('mouseleave', function(){
-  TweenMax.to($('.slick-track'), 0.4, {marginLeft: 0,ease: Quad.easeInOut});
-  TweenMax.to($('.slick-current'), 0.4, {x: 0,ease: Quad.easeInOut});
+  TweenMax.to($('.image-slider .slick-track'), 0.4, {marginLeft: 0,ease: Quad.easeInOut});
+  TweenMax.to($('.image-slider .slick-current'), 0.4, {x: 0,ease: Quad.easeInOut});
 });
 
+var tl1 = new TimelineMax();
+var $formCorner = $('.login-corner');
+var $header = $('header.header');
+var $banner = $('section.banner');
+
+tl1.from($header, .8, {y:-100, autoAlpha:0, ease:Power1.easeInOut})
+    .from($banner, .5, {y:100, autoAlpha:0, ease:Power1.easeInOut})
+    .from($formCorner, .5, {autoAlpha:0, y:200,ease:Quad.easeIn});
+
+// AOS INIT
+AOS.init();
+
+// sponsers slider
+
+$('.sponsers-slider').slick({
+  autoplay: true,
+  arrows: false,
+  dots: false,
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  pauseOnHover: false,
+  cssEase: 'cubic-bezier(0.84, 0, 0.08, 0.99)',
+})

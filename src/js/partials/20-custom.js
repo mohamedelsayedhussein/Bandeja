d3b06559f5442768d7250c823ff794661ece8c0e
MohamedElsayed24 <mohamedelsayedhussein0@gmail.com>
2019-12-10 17:59:17 +0200

    fdf

--- src/js/partials/20-custom.js
+++ src/js/partials/20-custom.js
@@ -203,31 +203,6 @@ $(document).ready(function(){
       }
     ]
   })
-    
-  // log in script
-    var panelOne = $('.form-panel.two').height(),
-        panelTwo = $('.form-panel.two')[0].scrollHeight + 200;
-  
-    $('.form-panel.two').not('.form-panel.two.active').on('click', function(e) {
-      e.preventDefault();
-      
-      $('.form-toggle').addClass('visible');
-      $('.form-panel.one').addClass('hidden');
-      $('.form-panel.two').addClass('active');
-      $('.form').animate({
-        'height': panelTwo
-      }, 200);
-    });
-  
-    $('.form-toggle').on('click', function(e) {
-      e.preventDefault();
-      $(this).removeClass('visible');
-      $('.form-panel.one').removeClass('hidden');
-      $('.form-panel.two').removeClass('active');
-      $('.form').animate({
-        'height': panelOne
-      }, 200);
-    });
 
     // PLAYER POSITION SCRIPT
 
@@ -247,12 +222,19 @@ $(document).ready(function(){
     })
 
     // BIRTHDATE INPUT INIT 
-      $( "#datepicker" ).dateDropper({
-        // large: true,
-        // lang: 'ar',
-        largeDefault: true,
-      });
+      $( "#datepicker" ).dateDropper();
 
+    // datatable script
+    $('#leaderboard-table').DataTable({
+      responsive: true,
+      // pagingType:'full_numbers', // last/first button
+      // searching: false,
+      // ordering: false,
+      // paging: false,
+      scrollCollapse: true,
+      scrollY: 400,
+      scrollX: true
+    });
 
 
 });
